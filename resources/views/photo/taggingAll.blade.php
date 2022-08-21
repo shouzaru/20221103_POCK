a55248ddb8716c1ba62e549e19816c6fe3acebf9
shouzaru <shouzaru@icloud.com>
2022-08-21 23:06:39 +0900

    11th commit

--- resources/views/photo/taggingAll.blade.php
+++ resources/views/photo/taggingAll.blade.php
@@ -104,7 +104,7 @@
                         @foreach ($players as $player)  <!-- 全選手から一つずつ取り出す -->
                             <label class="checkbox">
                                 <!-- onchange="submit(this.form)"で、チェックしたら送信 -->
-                                <input type="checkbox" name="players[]" value="{{$player->id}}" onchange="submit(this.form)&alert('タグを編集します')" 
+                                <input type="checkbox" name="players[]" value="{{$player->id}}" onchange="submit(this.form)&alert('タグを編集します')&location(this)" 
                                 @if(in_array($player->id , $photo->players->pluck('id')->toArray())) checked @endif> <!-- player_idとphotoが持っているplayer_idが一致したらチェック -->
                                 {{ $player->nickname }}
                             </label>
